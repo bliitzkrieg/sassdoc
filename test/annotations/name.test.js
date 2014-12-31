5e78ac51403428420e3b8dab1d7e452a26deae66
Fabrice Weinberg <Fabrice@weinberg.me>
2014-12-31 13:57:46 +0100

    first draft

---
+++ test/annotations/name.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('../init');
+
+var assert = require('assert');
+
+describe('#name', function () {
+  var name = (new (require('../../dist/annotation').default)()).list.name;
+
+  it('should return the trimmed string', function () {
+    assert.equal(name.parse('   '), '');
+    assert.equal(name.parse('   '), '');
+    assert.equal(name.parse('\ntest\t'), 'test');
+    assert.equal(name.parse('\nte\nst\t'), 'te\nst');
+  });
+});
